2026996f8110081df34a28589984c737ddffc83a
Akira Matsumoto <akiram8427@gmail.com>
2024-12-13 13:01:32 +0900

    Initial commit

---
+++ mypkg/random_number_generator.py
@@ -0,0 +1,25 @@
+# ランダムな数字を出力するためのノード
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Int16
+
+class talker(Node):
+    def __init__(self):
+        super().__init__("talker")
+        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        self.create_timer(0.5, self.cb)
+
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+
+def main():
+    rclpy.init()
+    node = Talker()
+    rclpy.spin(node)
