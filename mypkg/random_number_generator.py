ddb4532f33fea0c43012c8bc4efd4ede4146c19f
Akira Matsumoto <akiram8427@gmail.com>
2024-12-13 17:01:27 +0900

    方針変更前のpush

--- mypkg/random_number_generator.py
+++ mypkg/random_number_generator.py
@@ -7,19 +7,19 @@ from std_msgs.msg import Int16
 
 class Random(Node):
     def __init__(self):
-        super().__init__("random")
+        super().__init__("random_number_generator")
         self.pub = self.create_publisher(Int16, "random_number", 10)
         self.i = 1
         self.n = []
         while 1:
-            i += 1
-            self.n[i] = random.randrange(10)
+            self.i += 1
+            self.n.append(random.randrange(10))
         self.create_timer(0.05, self.cb)
 
 
     def cb(self):
         msg = Int16()
-        msg.data = self.n
+        msg.data = self.n[i]
         self.pub.publish(msg)
 
 
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'random = mypkg.random:main',
+            'random_number_generator = mypkg.random_number_generator:main',
         ],
     },
 )
