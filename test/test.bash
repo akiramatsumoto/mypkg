4f837ef57b1743acfb9c86d252e2ddf9310cb812
Akira Matsumoto <akiram8427@gmail.com>
2025-01-07 17:41:43 +0900

    change .github/workflows

--- test/test.bash
+++ test/test.bash
@@ -11,13 +11,11 @@ source $dir/.bashrc
 
 ros2 run mypkg battery_monitor &
 NODE_PID=$!
-timeout 60 ros2 topic echo batterylevel > - /tmp/mypkg.log
+percent=$(ros2 topic echo batterylevel --once | grep -oE '[0-9]+(\.[0-9]+)?')
 kill -9 $NODE_PID
 
-cat /tmp/mypkg.log
-percent=$(cat /tmp/mypkg.log | head -n 1)
 if (( $(echo "$percent >= 0" | bc -l) )) && (( $(echo "$percent <= 100" | bc -l) )); then
-    exit 0
+    echo 0
 else
     exit 1
 fi
